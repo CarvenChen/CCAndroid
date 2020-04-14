ef6fe91acb5e5604922d7581a7a4b3d49ce798f3
陈海华 <chenhaihua@znlhzl.com>
2020-04-14 15:25:04 +0800

    layout demo

--- CCDemo01/02-relativelayout/src/main/java/com/example/a02_relativelayout/MainActivity.java
+++ CCDemo01/02-relativelayout/src/main/java/com/example/a02_relativelayout/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a02_relativelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button updateButton = findViewById(R.id.update);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了立即更新", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button latterButton = findViewById(R.id.latter);
+        latterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了稍后提醒", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
