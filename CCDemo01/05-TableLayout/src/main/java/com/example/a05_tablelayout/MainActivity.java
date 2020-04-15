b21f986c78aec6b1dd97f226a06485de7cf33b71
陈海华 <chenhaihua@znlhzl.com>
2020-04-15 18:56:01 +0800

    更新demo

--- CCDemo01/05-TableLayout/src/main/java/com/example/a05_tablelayout/MainActivity.java
+++ CCDemo01/05-TableLayout/src/main/java/com/example/a05_tablelayout/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a05_tablelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button registerButton = findViewById(R.id.register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了注册", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了登录", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
