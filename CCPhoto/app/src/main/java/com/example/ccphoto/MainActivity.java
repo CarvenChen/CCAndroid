bbc0dc3e89a872cb68bc17d364356063e4e486c4
陈海华 <chenhaihua@znlh.com>
2020-08-04 19:45:12 +0800

    mod

--- CCPhoto/app/src/main/java/com/example/ccphoto/MainActivity.java
+++ CCPhoto/app/src/main/java/com/example/ccphoto/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.ccphoto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login);
+        registerButton = findViewById(R.id.register);
+
+        //登录
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //注册
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ CCPhoto/app/src/main/java/com/example/ccphoto/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.ccphoto.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ccphoto.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
