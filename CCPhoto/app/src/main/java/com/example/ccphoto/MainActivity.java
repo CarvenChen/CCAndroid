eb5eb8c61ecc1c53f7f5590bdec5e92a2c58141d
陈海华 <chenhaihua@znlh.com>
2020-08-17 11:14:50 +0800

    1

--- CCPhoto/app/src/main/java/com/example/ccphoto/MainActivity.java
+++ CCPhoto/app/src/main/java/com/example/ccphoto/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.ccphoto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ccphoto.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent in = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(in);
             }
         });
 
